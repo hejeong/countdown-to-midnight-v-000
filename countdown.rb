9cccf0fef6b2030fc7e0e34cb42042028d9a77d6
Jonathan Hong <jonhong777@gmail.com>
2018-07-02 21:39:28 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,11 @@ def countdown(number)
   "HAPPY NEW YEAR!"
 end
 
+def countdown(number)
+  while number > 0 do
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
+end
